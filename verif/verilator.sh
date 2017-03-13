d11ae6c208e60f3500365de3791f4340dc082aea
Alfred M. Szmidt <ams@gnu.org>
2017-03-13 15:41:55 +0000

    verilator.sh: Figure out verilator location from PATH.

--- verif/verilator.sh
+++ verif/verilator.sh
@@ -1,5 +1,4 @@
-V=/usr/local/src/verilator-3.854/bin/verilator
-#V=verilator
+V=verilator
 
 #$V -cc -LDFLAGS "-lSDL -lpthread" -exe --Mdir ./tmp --top-module test run-verilator.v ../verilator/test.cpp ../verilator/ide.cpp ../verilator/vga.cpp && \
 #(cd tmp; make OPT="-O2" -f Vtest.mk)
