9936dd432f335ced428068467f1ca03fb169054c
Alfred M. Szmidt <ams@gnu.org>
2017-08-05 16:54:26 +0000

    rtl/cadr/mskg4.v: Minor cleanup.

--- rtl/cadr/mskg4.v
+++ rtl/cadr/mskg4.v
@@ -19,6 +19,7 @@ module MSKG4(clk, mskl, mskr, msk);
 
    part_32x32prom_maskleft i_MSKR(.clk(~clk), .q(msk_left_out), .addr(mskl));
    part_32x32prom_maskright i_MSKL(.clk(~clk), .q(msk_right_out), .addr(mskr));
+
    assign msk = msk_right_out & msk_left_out;
 
 endmodule
