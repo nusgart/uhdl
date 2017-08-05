61fc104b6178c852a3b6aa36edb4b03f0647bc8d
Alfred M. Szmidt <ams@gnu.org>
2017-08-05 17:11:48 +0000

    rtl/cadr/amem01.v; Minor cleanup.

--- rtl/cadr/amem01.v
+++ rtl/cadr/amem01.v
@@ -13,6 +13,8 @@ module AMEM01(clk, reset, l, aadr, arp, awp, amem);
 
    ////////////////////////////////////////////////////////////////////////////////
 
-   part_1kx32dpram_a i_AMEM(.reset(reset), .clk_a(clk), .address_a(aadr), .data_a(32'b0), .q_a(amem), .wren_a(1'b0), .rden_a(arp), .clk_b(clk), .address_b(aadr), .data_b(l), .q_b(), .wren_b(awp), .rden_b(1'b0));
+   part_1kx32dpram_a i_AMEM(.reset(reset),
+                            .clk_a(clk), .address_a(aadr), .data_a(32'b0), .q_a(amem), .wren_a(1'b0), .rden_a(arp),
+                            .clk_b(clk), .address_b(aadr), .data_b(l), .q_b(), .wren_b(awp), .rden_b(1'b0));
 
 endmodule
