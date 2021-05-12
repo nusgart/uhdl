e91107b96e35d25bc049e1b153ade01c1897114d
Alfred Szmidt <ams@gnu.org>
2021-05-12 11:34:15 +0200

    cores/hz60.v: Update from Tumbleweed.

--- cores/hz60.v
+++ cores/hz60.v
@@ -3,39 +3,22 @@
 `timescale 1ns/1ps
 `default_nettype none
 
-module hz60(/*AUTOARG*/
-   // Outputs
-   hz60_clk_fired, hz60_clk,
-   // Inputs
-   clk, reset, hz60_enabled
-   );
+module hz60
+  (input wire 	     hz60_enabled,
+   output wire 	     hz60_clk_fired,
+   output reg [31:0] hz60_clk,
 
-   parameter
-     SYS_CLK = 26'd40_625_000,//26'd50000000,
-     HZ60_CLK_RATE = 26'd60,
-     HZ60_CLK_DIV = SYS_CLK / HZ60_CLK_RATE;
+   input wire 	     clk,
+   input wire 	     reset);
 
-   input wire clk;
-   input wire reset;
-
-   input wire hz60_enabled;
-   output wire hz60_clk_fired;
-   output [31:0] hz60_clk;
+   parameter SYS_CLK = 26'd40_625_000;//26'd50000000;
+   parameter HZ60_CLK_RATE = 26'd60;
+   parameter HZ60_CLK_DIV = SYS_CLK / HZ60_CLK_RATE;
 
    ////////////////////////////////////////////////////////////////////////////////
 
-   /*AUTOWIRE*/
-   /*AUTOREG*/
-   // Beginning of automatic regs (for this module's undeclared outputs)
-   reg [31:0]		hz60_clk;
-   // End of automatics
-
-   ////////////////////////////////////////////////////////////////////////////////
-
-   reg [19:0] hz60_counter;
-   wire [25:0] hz60_clk_div;
-
-   assign hz60_clk_div = HZ60_CLK_DIV;
+   reg [19:0] 	     hz60_counter;
+   wire [25:0] 	     hz60_clk_div = HZ60_CLK_DIV;
 
    always @(posedge clk)
      if (reset) begin
