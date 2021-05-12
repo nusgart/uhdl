2e16a988d2d78bfdc54c412f7663a396a2341077
Alfred Szmidt <ams@gnu.org>
2021-05-12 11:31:30 +0200

    boards/pipistrello.mk: Update from Tumbleweed.

--- boards/pipistrello.mk
+++ boards/pipistrello.mk
@@ -4,34 +4,35 @@ CONSTRAINTS	= boards/pipistrello.ucf
 
 PROGRAMMER	= fpgaprog
 
-XSTFLAGS	= -sd cores/xilinx
-NGDBUILDFLAGS	= -sd cores/xilinx
+XCORES		= boards/pipistrello/cores/xilinx
 
-TOPLEVEL	= top_lx45
+XSTFLAGS	= -sd $(XCORES)
+NGDBUILDFLAGS	= -sd $(XCORES)
+
+TOPLEVEL	= uhdl_pipistrello
 SYN_SRCS_V	+= \
-	top_lx45.v \
-	support_lx45.v \
-	ram_controller_lx45.v \
-	cores/xilinx/clk_wiz.v \
-	cores/xilinx/ise_MMEM.v \
-	cores/xilinx/ise_SPC.v \
-	cores/xilinx/ise_VMEM0.v \
-	cores/xilinx/ise_DRAM.v \
-	cores/xilinx/ise_PDL.v \
-	cores/xilinx/ise_AMEM.v \
-	cores/xilinx/ise_VMEM1.v \
-	cores/xilinx/ise_IRAM.v \
-	cores/xilinx/ise_vram.v \
-	cores/xilinx/mig_32bit/user_design/rtl/infrastructure.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mcb_controller/iodrp_controller.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mcb_controller/iodrp_mcb_controller.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mcb_controller/mcb_raw_wrapper.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mcb_controller/mcb_soft_calibration.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mcb_controller/mcb_soft_calibration_top.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mcb_controller/mcb_ui_top.v \
-	cores/xilinx/mig_32bit/user_design/rtl/memc_wrapper.v \
-	cores/xilinx/mig_32bit/user_design/rtl/mig_32bit.v
+	uhdl_pipistrello.v \
+	support_pipistrello.v \
+	ram_controller_pipistrello.v \
+	$(XCORES)/clk_wiz.v \
+	$(XCORES)/ise_MMEM.v \
+	$(XCORES)/ise_SPC.v \
+	$(XCORES)/ise_VMEM0.v \
+	$(XCORES)/ise_DRAM.v \
+	$(XCORES)/ise_PDL.v \
+	$(XCORES)/ise_AMEM.v \
+	$(XCORES)/ise_VMEM1.v \
+	$(XCORES)/ise_IRAM.v \
+	$(XCORES)/ise_vram.v \
+	$(XCORES)/mig_32bit/user_design/rtl/infrastructure.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mcb_controller/iodrp_controller.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mcb_controller/iodrp_mcb_controller.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mcb_controller/mcb_raw_wrapper.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mcb_controller/mcb_soft_calibration.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mcb_controller/mcb_soft_calibration_top.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mcb_controller/mcb_ui_top.v \
+	$(XCORES)/mig_32bit/user_design/rtl/memc_wrapper.v \
+	$(XCORES)/mig_32bit/user_design/rtl/mig_32bit.v
 
 # support_tb.v
-isim: TESTBENCHES += top_lx45_tb
-
+isim: TESTBENCHES += uhdl_pipistrello_tb
